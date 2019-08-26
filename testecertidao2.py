da15c6904eb798fafce7189c82bcc013b2f5a635
Bruno de Moraes <brunoestudolog@gmail.com>
2019-08-26 14:36:47 -0300

    Arquivo principal de execução

---
+++ testecertidao2.py
@@ -0,0 +1,43 @@
+from certidao2 import Certidao, Uniao, Tst, Fgts, Gdf
+
+dia = '06'
+mes = '08'
+ano = '2019'
+orgaos = ['UNIAO', 'TST', 'FGTS', 'GDF']
+empresasdic = {}
+
+
+obj1 = Certidao(dia, mes, ano)
+lis_ref_cel = obj1.pega_referencia()
+if len(lis_ref_cel) == 0:
+    raise Exception('Data não encontrada!')
+elif len(lis_ref_cel) > 1:
+    raise Exception(f'A data especificada foi encontrada nas células {lis_ref_cel} da planilha de pagamentos.'
+          f'\nApague os valores duplicados e execute o programa novamente.')
+else:
+    ref_cel = lis_ref_cel[0]
+fornecedores = obj1.pega_fornecedores(ref_cel)
+obj1.cria_diretorio(fornecedores)
+obj1.certidoes_n_encontradas(fornecedores, orgaos)
+obj1.pdf_para_jpg(fornecedores, orgaos)
+objUniao = Uniao(dia, mes, ano)
+objTst = Tst(dia, mes, ano)
+objFgts = Fgts(dia, mes, ano)
+objGdf = Gdf(dia, mes, ano)
+lista_objetos = [objUniao, objTst, objFgts, objGdf]
+
+for emp in fornecedores:
+    empresadic = {}
+    index = 0
+    for objeto in lista_objetos:
+        cert = objeto.pega_string(emp)
+        val = objeto.confere_data(cert)
+        if val == True:
+            empresadic[orgaos[index]] = 'OK'
+        else:
+            empresadic[orgaos[index]] = 'Certidão não compreende data de pagamento'
+        index += 1
+    print(empresadic)
+    empresasdic[emp] = empresadic
+
+
