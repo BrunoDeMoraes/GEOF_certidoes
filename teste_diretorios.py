061768bd30dfe998a3835944c74e0388bcde185e
Bruno de Moraes <brunoestudolog@gmail.com>
2019-09-02 14:39:26 -0300

    teste em string de data

--- teste_diretorios.py
+++ teste_diretorios.py
@@ -1,15 +1,10 @@
 from diretorios import Diretorio
 
-pasta_mae = '//hrg-74977/GEOF/CERTIDÕES/Teste_renomear'
+pasta_mae = '//hrg-74977/GEOF/CERTIDÕES/CERTIDÕES GERAL - Cópia'
 
 checagem = Diretorio(pasta_mae)
 lista_de_pastas = checagem.lista_pastas()
 print(lista_de_pastas)
 lista_de_arqui = checagem.lista_arquivos(lista_de_pastas)
 print(lista_de_arqui)
-checagem.renomeia_arquivos(lista_de_arqui, '2019-08-26')
-
-
-
-
-
+checagem.renomeia_arquivos(lista_de_arqui, ('data teste'))
