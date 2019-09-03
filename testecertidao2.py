4607207495cae1245ed8572ef5f50f388e79dac8
Bruno de Moraes <brunoestudolog@gmail.com>
2019-09-03 19:12:57 -0300

    Novas inserções de mensagem de log

--- testecertidao2.py
+++ testecertidao2.py
@@ -1,14 +1,16 @@
 from certidao2 import Certidao, Uniao, Tst, Fgts, Gdf
+import time
 
-dia = '02'
-mes = '09'
+tempo_inicial = time.time()
+dia = '26'
+mes = '08'
 ano = '2019'
 orgaos = ['UNIAO', 'TST', 'FGTS', 'GDF']
 empresasdic = {}
 
 
 obj1 = Certidao(dia, mes, ano)
-obj1.mensagem_log('\nInício da execução')
+obj1.mensagem_log('Início da execução')
 lis_ref_cel = obj1.pega_referencia()
 if len(lis_ref_cel) == 0:
     obj1.mensagem_log('\nData específicada não encontrada')
@@ -19,11 +21,11 @@ elif len(lis_ref_cel) > 1:
           f'\nApague os valores duplicados e execute o programa novamente.')
 else:
     ref_cel = lis_ref_cel[0]
-    obj1.mensagem_log(f'Referência encontrada na céclula {lis_ref_cel[0]}')
+    obj1.mensagem_log(f'\nReferência encontrada na célula {lis_ref_cel[0]}')
 fornecedores = obj1.pega_fornecedores(ref_cel)
-obj1.mensagem_log('\n\nFornecedores analisados:')
+obj1.mensagem_log_sem_horario('\nFornecedores analisados:')
 for emp in fornecedores:
-    obj1.mensagem_log(f'\n{emp}')
+    obj1.mensagem_log_sem_horario(f'{emp}')
 obj1.cria_diretorio(fornecedores)
 obj1.certidoes_n_encontradas(fornecedores, orgaos)
 obj1.pdf_para_jpg(fornecedores, orgaos)
@@ -33,13 +35,14 @@ objFgts = Fgts(dia, mes, ano)
 objGdf = Gdf(dia, mes, ano)
 lista_objetos = [objUniao, objTst, objFgts, objGdf]
 
+obj1.mensagem_log('\nInicio da conferência de datas de emissão e vencimento:')
 for emp in fornecedores:
     empresadic = {}
     index = 0
     for objeto in lista_objetos:
         cert = objeto.pega_string(emp)
-        print(f'Tudo certo até aqui {emp} {objeto}')
-        objeto.mensagem_log(f'\npra {emp}')
+        print(f'Processando string {emp}')
+        obj1.mensagem_log(f'\n{emp}')
         val = objeto.confere_data(cert)
         if val == True:
             empresadic[orgaos[index]] = 'OK'
@@ -48,7 +51,13 @@ for emp in fornecedores:
         index += 1
     empresasdic[emp] = empresadic
 print('Resultado da conferência:')
+obj1.mensagem_log_sem_horario('\nResultado da conferência:')
 numerador = 0
 for emp in empresasdic:
-    print(f'{numerador} - {emp} - {empresasdic[emp]}')
+    print(f'{numerador + 1} - {emp} - {empresasdic[emp]}')
+    obj1.mensagem_log(f'{numerador + 1} - {emp} - {empresasdic[emp]}')
     numerador += 1
+tempo_final = time.time()
+tempo_de_execução = int((tempo_final - tempo_inicial))
+obj1.mensagem_log(f'\nTempo total de execução: {tempo_de_execução // 60} minutos e {tempo_de_execução % 60} segundos.')
+obj1.mensagem_log_sem_horario('\n\n======================================================================================\n')
