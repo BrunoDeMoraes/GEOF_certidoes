f0847f079d85ffda89da9b1415a71ec86e4f32a2
Bruno de Moraes <brunoestudolog@gmail.com>
2019-11-11 18:07:16 -0300

    Fechamento desse arquivo de teste

--- teste_diretorios.py
+++ teste_diretorios.py
@@ -1,10 +1,9 @@
 from diretorios import Diretorio
 
-pasta_mae = '//hrg-74977/GEOF/CERTIDÕES/CERTIDÕES GERAL - Cópia'
+pasta_mae = '//hrg-74977/GEOF/CERTIDÕES/Testes/Teste_renomear'
 
 checagem = Diretorio(pasta_mae)
 lista_de_pastas = checagem.lista_pastas()
 print(lista_de_pastas)
 lista_de_arqui = checagem.lista_arquivos(lista_de_pastas)
 print(lista_de_arqui)
-checagem.renomeia_arquivos(lista_de_arqui, ('data teste'))
