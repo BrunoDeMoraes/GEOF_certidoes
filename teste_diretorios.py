bf3e5ae2a21f81e5e01a0e9dd77d06c61020aa2b
Bruno de Moraes <brunoestudolog@gmail.com>
2020-02-03 21:05:18 -0300

    arquivo deletado do projeto

--- teste_diretorios.py
+++
@@ -1,9 +0,0 @@
-from diretorios import Diretorio
-
-pasta_mae = '//hrg-74977/GEOF/CERTIDÕES/Testes/Teste_renomear'
-
-checagem = Diretorio(pasta_mae)
-lista_de_pastas = checagem.lista_pastas()
-print(lista_de_pastas)
-lista_de_arqui = checagem.lista_arquivos(lista_de_pastas)
-print(lista_de_arqui)
